5f1d2b9e64a9f5b382b05d57868fb96c9e5576be
1wooseok <kingseok0014@gmail.com>
2024-09-02 14:31:01 +0900

    fix: root path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/<REPO>/',
+  // base: '/<REPO>/',
   build: {
     rollupOptions: {
       input: {
